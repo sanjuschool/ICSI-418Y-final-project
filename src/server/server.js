c82431e103f96ec6a588fc3a5da240a809483c5d
Mamadou325 <mamadouwague805@gmail.com>
2026-04-15 19:15:39 -0400

    Adding database connection files

---
+++ src/server/server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const mongoString = "mongodb+srv://418User:418Password@cluster0.kuurxla.mongodb.net/ua_maps?appName=Cluster0";
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => console.log(error))
+database.once('connected', () => console.log('Database Connected'))
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    username: String,
+    password: String,
+    role: String
+});
+
+const User = mongoose.model('User', userSchema, 'Users');
+
+app.post('/createUser', async (req, res) => {
+    console.log(`SERVER: CREATE USER REQ BODY: ${req.body.username} ${req.body.firstName} ${req.body.lastName}`)
+    const un = req.body.username
+    try {
+        User.exists({username: un}).then(result => {
+            if(Object.is(result, null)) {
+                const user = new User(req.body);
+                user.save()
+                console.log(`User created! ${user}`)
+                res.send(user)
+            } else {
+                console.log("Username already exists")
+                res.status(500).send("Username already exists")
+            }
+        })
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.get('/getUser', async (req, res) => {
+    const username = req.query.username
+    const password = req.query.password
+    
+    console.log(username)
+    console.log(password)
+
+    try {
+        const user = await User.findOne({ username, password })
+        if (user) {
+            res.send(user)
+        } else {
+            res.status(401).send("Login failed, try again.")
+        }
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+app.listen(9000, () => {
+    console.log(`Server Started at ${9000}`)
+});
+
+
+
+
